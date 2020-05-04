722b514c957fd645a41221d210ef72d21b77e297
Dana Kiehl <dana.palm87@gmail.com>
2020-05-04 10:51:55 -0500

    Added a MarketingFirm, SweepstakesStackManager, SweepstakesQueueManager, Sweepstakes, and Contestant class. Created an interface ISweepstakesManger

---
+++ SweepStakes/Contestant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Contestant
+    {
+
+        //member variables
+        public string FirstName;
+        public string LastName;
+        public string EmailAddress;
+        public int RegistNumber;
+
+        //constructor
+        public Contestant()
+        {
+
+        }
+
+        //member methods
+
+    }
+}
---
+++ SweepStakes/Sweepstakes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class Sweepstakes
+    {
+        //member variables
+
+        //constructor
+        public Sweepstakes(string name)
+        {
+
+        }
+
+        //member methods
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+        public Contestant PickWinner()
+        {
+            return Contestant;
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
